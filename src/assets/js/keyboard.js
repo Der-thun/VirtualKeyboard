5eba3396272a7eeb03967ac6d5ceaa87c21a8286
Boris Kortunov <boris.kortunov@gmail.com>
2022-05-10 01:15:35 +0300

    feat: change language

--- src/assets/js/keyboard.js
+++ src/assets/js/keyboard.js
@@ -6,9 +6,9 @@
     'Ctrl', 'Win', 'Alt', 'Space', 'Alt', 'Left', 'Down', 'Right', 'Ctrl'
 ]
 
-const dictRuShiftFirstLine = [
-    'Ё', '!', '"', '№', ';', '%', ':', '?', '*', '(', ')', '_', '+' 
-]
+const dictRuShift = {
+    0: 'Ё', 1: '!', 2: '"', 3: '№', 4: ';', 5: '%', 6: ':', 7: '?', 8: '*', 9: '(', 10: ')', 11: '_', 12: ='+' 
+}
 
 
 const witchKey = {
@@ -28,7 +28,7 @@ const dictEn = [
 ];
 
 const dictEnShiftFirstLine = [
-    '¬', '!', '"', '£', '$', '%', '^', '&', '*', '(', ')', '_', '+' 
+    0: '~', 1: '!', 2: '@', 3: '#', 4: '$', 5: '%', 6: '^', 7: '&', 8: '*', 9: '(', 10: ')', 11: '_', 12: '+' 
 ];
 
 */
@@ -54,6 +54,9 @@ export class Keyboard  {
         capsLock: false,
         shift: false,
         shiftReal: false,
+        alt: false,
+        ctrl: false,
+        currentLanguage: 'ru',
         dictRu: [
             'ё', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace',
             'Tab', 'й', 'ц', 'у', 'к', 'е', 'н', 'г', 'ш', 'щ', 'з', 'х', 'ъ', "\\", 'Del',
@@ -61,20 +64,42 @@ export class Keyboard  {
             'ShiftL', 'я', 'ч', 'с', 'м', 'и', 'т', 'ь', 'б', 'ю', '.', '▲', 'ShiftR',
             'CtrlL', 'Win', 'AltL', 'Space', 'AltR', '◄', '▼', '►', 'CtrlR'
         ],
-        dictRuShiftFirstLine: [
-            'Ё', '!', '"', '№', ';', '%', ':', '?', '*', '(', ')', '_', '+' 
+
+        dictRuShiftFirstLine: {
+            0: 'Ё', 1: '!', 2: '"', 3: '№', 4: ';', 5: '%', 6: ':', 7: '?', 8: '*', 9: '(', 10: ')', 11: '_', 12: '+', 27: '/', 52: ','
+        },
+
+        dictEn: [
+            "`", '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace',
+            'Tab', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', "\\", 'Del',
+            'CapsLock', "a", "s", "d", 'f', 'g', 'h', 'j', 'k', 'l', ';', "'", 'Enter', 
+            'Shift', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/',  '▲', 'Shift',
+            'Ctrl', 'Win', 'Alt', 'Space', 'Alt', '◄', '▼', '►', 'Ctrl'
         ],
+
+        dictEnShiftFirstLine: {
+            0: '~', 1: '!', 2: '@', 3: '#', 4: '$', 5: '%', 6: '^', 7: '&', 8: '*', 9: '(', 10: ')', 11: '_', 12: '+', 25: '{', 26: '}', 27: '/', 39: ':', 40: '"', 50: '<', 51: '>', 52: '?'
+        },
+
 
         unToggledKeys: [
             'backspace', 'tab', 'del', 'capslock', 'enter', 'shift', 'ctrl', 'win', 'alt', 'space'
         ],
 
-        whichKey: {
-            220: 'ё', 49: '1', 50: '2', 51: '3', 52: '4', 53: '5', 54: '6', 55: '7', 56: '8', 57: '9', 48: '0', 187: '-', 219: '=', /*8: 'backspace',*/
-            /*9: 'tab',*/ 81: 'й', 87: 'ц', 69: 'у', 82: 'к', 84: 'е', 89: 'н', 85: 'г', 73: 'ш', 79: 'щ', 80: 'з', 221: 'х', 186: 'ъ', 191: '\\',// 46: 'del',
-            /*20: 'capslock', */65: 'ф', 83: 'ы', 68: 'в', 70: 'а', 71: 'п', 72: 'р', 74: 'о', 75: 'л', 76: 'д', 192: 'ж', 222: 'э',// 13: 'enter',
-            /*16: 'shift',*/ 90: 'я', 88: 'ч', 67: 'с', 86: 'м', 66: 'и', 78: 'т', 77: 'ь', 188: 'б', 190: 'ю', 189: '.', 38: '▲',/* 16: 'shift',*/
-            /*17: 'ctrl', 91: 'Win', 18: 'alt', 32: 'space', */37: '◄', 40: '▼', 39: '►',/* 17: 'ctrl'*/
+        whichKeyRU: {
+            220: 'ё', 49: '1', 50: '2', 51: '3', 52: '4', 53: '5', 54: '6', 55: '7', 56: '8', 57: '9', 48: '0', 187: '-', 219: '=',
+            81: 'й', 87: 'ц', 69: 'у', 82: 'к', 84: 'е', 89: 'н', 85: 'г', 73: 'ш', 79: 'щ', 80: 'з', 221: 'х', 186: 'ъ', 191: '\\',
+            65: 'ф', 83: 'ы', 68: 'в', 70: 'а', 71: 'п', 72: 'р', 74: 'о', 75: 'л', 76: 'д', 192: 'ж', 222: 'э',
+            90: 'я', 88: 'ч', 67: 'с', 86: 'м', 66: 'и', 78: 'т', 77: 'ь', 188: 'б', 190: 'ю', 189: '.', 38: '▲',
+            37: '◄', 40: '▼', 39: '►',
+        },
+
+        whichKeyEN: {
+            220: '`', 49: '1', 50: '2', 51: '3', 52: '4', 53: '5', 54: '6', 55: '7', 56: '8', 57: '9', 48: '0', 187: '-', 219: '=',
+            81: 'q', 87: 'w', 69: 'e', 82: 'r', 84: 't', 89: 'y', 85: 'u', 73: 'i', 79: 'o', 80: 'p', 221: '[', 186: ']', 191: '\\',
+            65: 'a', 83: 's', 68: 'd', 70: 'f', 71: 'g', 72: 'h', 74: 'j', 75: 'k', 76: 'l', 192: ';', 222: "'",
+            90: 'z', 88: 'x', 67: 'c', 86: 'v', 66: 'b', 78: 'n', 77: 'm', 188: ',', 190: '.', 189: '/', 38: '▲',
+            37: '◄', 40: '▼', 39: '►',
         },
 
         codeKey: {            
@@ -96,21 +121,6 @@ export class Keyboard  {
             ControlRight: 'Ctrl'
         },
 
-        codeArray : [
-            'Backspace',
-            'Tab',  
-            'Delete',
-            'CapsLock',  
-            'Enter',
-            'ShiftLeft',
-            'ShiftRight',
-            'ControlLeft',
-            'MetaLeft',
-            'AltLeft',
-            'Space',
-            'AltRight',
-            'ControlRight'
-        ]
     }
 
     init() {
@@ -216,14 +226,19 @@ export class Keyboard  {
                 case 'CtrlL':
                     keyElement.classList.add('l', 'ctrlleft');
                     keyElement.textContent = 'Ctrl';
-
-                    this._ctrlKeyPress();
+                    keyElement.addEventListener('click', () => {
+                        this._ctrlKeyPress();
+                        this._changeLanguage()                        
+                    });
+                    
                     break;
                 case 'CtrlR':
                     keyElement.classList.add('l', 'ctrlright');
                     keyElement.textContent = 'Ctrl';
-
-                    this._ctrlKeyPress();
+                    keyElement.addEventListener('click', () => {
+                        this._ctrlKeyPress();                        
+                    });
+                    
                     break;
                 case 'Win':
                     keyElement.classList.add('l');
@@ -231,14 +246,19 @@ export class Keyboard  {
                 case 'AltL':
                     keyElement.classList.add('l', 'altleft');
                     keyElement.textContent = 'Alt';
-
-                    this._altKeyPress();
+                    keyElement.addEventListener('click', () => {
+                        this._altKeyPress();
+                        this._changeLanguage()                        
+                    });
+                    
                     break;
                 case 'AltR':
                     keyElement.classList.add('l', 'altright');
                     keyElement.textContent = 'Alt';
-
-                    this._altKeyPress();
+                    keyElement.addEventListener('click', () => {
+                        this._altKeyPress();                        
+                    });
+                    
                     break;
                 case 'Space':
                     keyElement.classList.add('space');
@@ -277,7 +297,8 @@ export class Keyboard  {
         let position = textarea.selectionStart;
         let positionEnd = textarea.selectionEnd;
         let text = this.properties.value;
-        let letter = event.target.classList.contains('key') ? event.target.textContent : (this.properties.shiftReal ? this.properties.whichKey[event.which].toUpperCase() : this.properties.whichKey[event.which].toLowerCase())
+        let whichKey = (this.properties.currentLanguage === 'ru') ? this.properties.whichKeyRU : this.properties.whichKeyEN
+        let letter = event.target.classList.contains('key') ? event.target.textContent : (this.properties.capsLock ? whichKey[event.which].toUpperCase() :  whichKey[event.which].toLowerCase() )
        
         if (text.length + 1 !== position) {
             this.properties.value = text.slice(0, position) + letter + text.slice(positionEnd);
@@ -340,43 +361,87 @@ export class Keyboard  {
     }
 
     _enterKeyPress() {
-        this._input('read');
-        this.properties.value += '\n';
-        this._input('write');
+        let textarea = document.querySelector('.input');
+        let position = textarea.selectionStart;
+        let positionEnd = textarea.selectionEnd;
+        let text = this.properties.value;
+        
+       
+        if (text.length + 1 !== position) {
+            this.properties.value = text.slice(0, position) + '\n' + text.slice(positionEnd);
+        } else {
+            this.properties.value += letter;
+        }
+        textarea.value = this.properties.value;
+        textarea.setSelectionRange(position + 1, position + 1);
     }
 
     _spaceKeyPress() {
-        this._input('read');
-        this.properties.value += ' ';
-        this._input('write');
+        let textarea = document.querySelector('.input');
+        let position = textarea.selectionStart;
+        let positionEnd = textarea.selectionEnd;
+        let text = this.properties.value;
+        
+       
+        if (text.length + 1 !== position) {
+            this.properties.value = text.slice(0, position) + ' ' + text.slice(positionEnd);
+        } else {
+            this.properties.value += letter;
+        }
+        textarea.value = this.properties.value;
+        textarea.setSelectionRange(position + 1, position + 1);
+        
     }
 
     _tabKeyPress() {
-        this._input('read');
-        this.properties.value += '    ';
-        this._input('write');
+        let textarea = document.querySelector('.input');
+        let position = textarea.selectionStart;
+        let positionEnd = textarea.selectionEnd;
+        let text = this.properties.value;
+        
+       
+        if (text.length + 1 !== position) {
+            this.properties.value = text.slice(0, position) + '    ' + text.slice(positionEnd);
+        } else {
+            this.properties.value += letter;
+        }
+        textarea.value = this.properties.value;
+        textarea.setSelectionRange(position + 4, position + 4);
     }
 
     _altKeyPress() {
-
+        this.properties.alt = !this.properties.alt
     }
 
     _ctrlKeyPress() {
-
+        this.properties.ctrl = !this.properties.ctrl
     }
 
-    _shift() {
-        console.log('!!!')
-        this.properties.shift = !this.properties.shift; 
-        for (let i = 0; i < this.properties.dictRuShiftFirstLine.length; i++)  {            
-            if (this.elements.keys[i].textContent === this.properties.dictRu[i]) {
-                this.elements.keys[i].textContent = this.properties.dictRuShiftFirstLine[i];
-            } else {
-                this.elements.keys[i].textContent = this.properties.dictRu[i];
-            }       
+    _shift() {  
+        this.properties.shift = !this.properties.shift;
+        
+        if (this.properties.currentLanguage === 'ru') {
+            for (let i in this.properties.dictRuShiftFirstLine)  {            
+                if (this.elements.keys[i].textContent === this.properties.dictRu[i]) {
+                    this.elements.keys[i].textContent = this.properties.dictRuShiftFirstLine[i];
+                } else {
+                    this.elements.keys[i].textContent = this.properties.dictRu[i];
+                }       
+            }
+            
+            this._toggleCaps()
+        } else {
+            for (let i in this.properties.dictEnShiftFirstLine)  {            
+                if (this.elements.keys[i].textContent === this.properties.dictEn[i]) {
+                    this.elements.keys[i].textContent = this.properties.dictEnShiftFirstLine[i];
+                } else {
+                    this.elements.keys[i].textContent = this.properties.dictEn[i];
+                }       
+            }
+            
+            this._toggleCaps()            
         }
         
-        this._toggleCaps()
     }
 
     _input(type) {
@@ -400,12 +465,10 @@ export class Keyboard  {
     _keyPress() {        
         document.querySelector('.input').addEventListener('keydown', (e) => {
             e.preventDefault();
-            let keysSet = document.querySelectorAll('.key');           
-           // console.log(e.code)
+            let keysSet = document.querySelectorAll('.key');
+            let whichKey = (this.properties.currentLanguage === 'ru') ? this.properties.whichKeyRU : this.properties.whichKeyEN
             keysSet.forEach(elem => {
-               // console.log(e.code)
                 if (this.properties.codeKey[e.code] === elem.textContent) {
-                //if (this.properties.codeArray.includes(e.code)) {   
                     console.log(e.code)
                     //console.log(elem.textContent)
                     
@@ -415,17 +478,15 @@ export class Keyboard  {
                         case "Enter" : this._enterKeyPress(); elem.classList.add('active'); break;
                         case 'Shift':
                             if (e.code === 'ShiftLeft') {
-                                document.querySelector('.shiftleft').classList.add('active');
-                                
+                                document.querySelector('.shiftleft').classList.add('active');                                
                                 
                             } else {
-                                document.querySelector('.shiftright').classList.add('active');
-                                
+                                document.querySelector('.shiftright').classList.add('active');                                
                                 
                             }
                             if (!this.properties.shiftReal) {
                                 this.properties.shiftReal = true;
-                                this._shift()
+                                this._shift()                                
                             }
                             
                             break;
@@ -436,7 +497,7 @@ export class Keyboard  {
                             break;
                         case 'Tab': this._tabKeyPress(); elem.classList.add('active');break;
                         case 'Ctrl': 
-                            if (e.code === 'ControlLeft') {
+                            if (e.code === 'ControlLeft') {                                
                                 document.querySelector('.ctrlleft').classList.add('active');                            
                             } else {
                                 document.querySelector('.ctrlright').classList.add('active');
@@ -459,34 +520,8 @@ export class Keyboard  {
                         default: break;
                     }
                     
-                } else if (elem.textContent.toLowerCase() === this.properties.whichKey[e.which]) {                    
-                    switch (elem.textContent) {
-                        //case "Enter" : this._enterKeyPress(); break;
-                        /*case 'Shift':
-                            if (!this.properties.shift) {                               
-                                this._shift();
-                            }
-                            break;
-                        */
-                        //case "Space": this._spaceKeyPress(); break;
-                        /*case 'CapsLock': 
-                            this._toggleCaps();
-                            elem.classList.toggle('caps-active'); 
-                            break;
-                            */
-                        //case 'Backspace': this._backspaceKeyPress(); break;
-                        /*case 'Tab': this._tabKeyPress(); break;
-                        case 'Ctrl': this._ctrlKeyPress(); break;
-                        case 'Win': break;
-                        case 'Alt': this._altKeyPress(); break;
-                        case 'Del': this._delKeyPress(); break;*/
-                        default: 
-                            this._defaultKeyPress(e);
-                            if (this.properties.shift) {
-                               // this._shift();
-                            }
-                    }                   
-                    
+                } else if (elem.textContent.toLowerCase() === whichKey[e.which]) {                    
+                    this._defaultKeyPress(e);
                     elem.classList.add('active')
                 } 
             })
@@ -494,6 +529,7 @@ export class Keyboard  {
 
         document.querySelector('.input').addEventListener('keyup', (e) => {
             let keysSet = document.querySelectorAll('.key');
+            let whichKey = (this.properties.currentLanguage === 'ru') ? this.properties.whichKeyRU : this.properties.whichKeyEN
             keysSet.forEach(elem => {
                 if (this.properties.codeKey[e.code] === elem.textContent) {
                     elem.classList.remove('active');
@@ -502,12 +538,36 @@ export class Keyboard  {
                         this.properties.shiftReal = false
                     }
                     
-                } else if (elem.textContent.toLowerCase() === this.properties.whichKey[e.which]) {
+                } else if (elem.textContent.toLowerCase() === whichKey[e.which]) {
                    
                     elem.classList.remove('active')
                 }
             })
         })
+    }
+
+
+    _changeLanguage() {
+        
+        if (this.properties.currentLanguage === 'ru') {
+            for (let i = 0; i < this.elements.keys.length; i++) {
+                let element = this.elements.keys[i].textContent 
+                if (element !== 'Shift' && element !== 'Alt' && element !== 'Ctrl') {
+                    this.elements.keys[i].textContent = this.properties.dictEn[i]
+                }                
+                
+            }
+            this.properties.currentLanguage = 'en'
+        } else {
+            for (let i = 0; i < this.elements.keys.length; i++) { 
+                let element = this.elements.keys[i].textContent
+                if (element !== 'Shift' && element !== 'Alt' && element !== 'Ctrl') {               
+                    this.elements.keys[i].textContent = this.properties.dictRu[i]
+                }
+            }
+            this.properties.currentLanguage = 'ru'            
+        }
+        
     }
 };
 
