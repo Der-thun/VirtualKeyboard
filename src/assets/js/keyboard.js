f1e006d13b1e112347f87003ee9549d1c30c8196
Boris Kortunov <boris.kortunov@gmail.com>
2022-05-10 03:09:16 +0300

    fix: button size

--- src/assets/js/keyboard.js
+++ src/assets/js/keyboard.js
@@ -42,7 +42,7 @@ export class Keyboard  {
             "`", '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace',
             'Tab', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', "\\", 'Del',
             'CapsLock', "a", "s", "d", 'f', 'g', 'h', 'j', 'k', 'l', ';', "'", 'Enter', 
-            'ShiftR', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/',  '▲', 'ShiftL',
+            'ShiftL', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/',  '▲', 'ShiftR',
             'CtrlL', 'Win', 'AltR', 'Space', 'AltL', '◄', '▼', '►', 'CtrlR'
         ],
 
